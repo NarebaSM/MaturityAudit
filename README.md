5f8ffbc5061cd9e15e36898899f04181b0e1e4b1
Paulo <pcontatop@gmail.com>
2023-11-12 21:03:10 -0300

    add graphics in response gerate, add README, add venv instructions, add requirements

---
+++ README.md
@@ -0,0 +1,34 @@
+# MaturityAudit Project
+
+## Overview
+
+Projeto para Fatec sobre auditoria
+
+## Prerequisites
+
+You need to use Python 3.7 to run this project
+https://www.python.org/downloads/release/python-370/
+
+1. Clone this repository:
+
+```
+$ git clone https://github.com/NarebaSM/MaturityAudit.git
+```
+
+2. For execute the project you need to execute the venv(virtual environment)
+
+```
+$ cd venv/bin/python/bin
+```
+
+AND
+
+```
+$ source activate
+```
+
+Before getting started, make sure you have installed requirements by this command:
+
+```
+$ pip install -r requirements.txt
+```
--- authorization.py
+++ authorization.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, flash, request, redirect, url_for, jsonify, abort, make_response, Response
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort, make_response
 from flask_cors import CORS
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-import socket
-import json
-import jwt
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
 import datetime
 import hashlib
+import json
+import jwt
+import os
+
 import base64
 
 
@@ -19,6 +20,7 @@ def generate_token(username, secret):
     }
     token = jwt.encode(payload, secret, algorithm='HS256')
     return token
+
 
 def generate_secret_key(username, password):
     # Combine username e password para criar uma chave única
@@ -42,11 +44,10 @@ def getUserItems(username):
         if validate == "no":
             return None
 
+
 def sendMail(email):
     remetente = "no-reply.segma5@change.pass"
     destinatario = email
-
-
 
 
 # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -54,18 +55,20 @@ def sendMail(email):
 # ip = s.getsockname()[0]
 # s.close()
 app = Flask(__name__)
-CORS(app, supports_credentials=True, origins=['auditsegma5.ddns.net', '191.182.179.92:9876'], headers=['Content-Type', 'Authorization', 'secret'])
+CORS(app, supports_credentials=True, origins=['auditsegma5.ddns.net', '191.182.179.92:9876'],
+     headers=['Content-Type', 'Authorization', 'secret'])
 app.config['SESSION_COOKIE_DOMAIN'] = 'auditsegma5.ddns.net'
 
 app.secret_key = 'achavedosucessoeosucesso'
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-
     if request.method == 'GET':
         return render_template('index.html')
     else:
@@ -91,7 +94,6 @@ def login():
 
 @app.route('/forms', methods=['GET'])
 def forms():
-
     token = request.cookies.get('Authorization')
 
     if token and token.startswith('Bearer'):
@@ -119,20 +121,75 @@ def forms():
     else:
         abort(401, description="Token não fornecido. Faça login primeiro.")
 
-
 @app.route('/submit', methods=['POST'])
 def submit():
     count = 0
     userResp = {}
-    while count < 3:
+
+    while count < 3: #alterar a constante para a quantidade atual de de perguntas
         userResp[f"questao{count}"] = request.form.get(f"nivel{count}")
-        count = count +1
-    print(f"user resp = {userResp}")
-    return "Request Submited :)"
+        count = count + 1
+
+    existing_data = {}
+    if os.path.exists('response.json'):
+        with open('response.json', 'r') as file:
+            existing_data = json.load(file)
+
+    if not isinstance(existing_data, dict):
+        existing_data = {}
+
+    for key, value in userResp.items():
+        existing_data[key] = value
+
+
+    with open('response.json', 'w') as file:
+        json.dump(existing_data, file)
+
+    print(f"Respostas salvas: {existing_data}")
+
+    return redirect(url_for('chart'))
 
 @app.route('/register', methods=['GET'])
 def register():
     return render_template('register.html')
+
+
+def generate_chart():
+
+    existing_data = {}
+    if os.path.exists('response.json'):
+        with open('response.json', 'r') as file:
+            existing_data = json.load(file)
+
+    counts = {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0}
+    total_responses = 0
+
+    for key, value in existing_data.items():
+        if key.startswith('questao') and value in counts:
+            counts[value] += 1
+            total_responses += 1
+
+    averages = {k: v / total_responses for k, v in counts.items()}
+
+    # Gerar gráfico de barras
+    plt.bar(averages.keys(), averages.values())
+    plt.xlabel('Nota')
+    plt.ylabel('Frequência Média')
+    plt.title('Média de Notas')
+
+    # Converter a imagem em formato base64
+    img_data = BytesIO()
+    plt.savefig(img_data, format='png')
+    img_data.seek(0)
+    img_base64 = base64.b64encode(img_data.read()).decode('utf-8')
+
+    return img_base64
+
+
+@app.route('/chart')
+def chart():
+    img_base64 = generate_chart()
+    return render_template('chart.html', img_base64=img_base64)
 
 @app.route('/registerSub', methods=['POST'])
 def registerSub():
@@ -156,12 +213,14 @@ def registerSub():
             secret = generate_secret_key(usernameReg, passwordReg)
             with open('pass.json', 'r') as passArq:
                 pass_data = json.load(passArq)
-            newPass = {"username": f"{usernameReg}", "email": f"{emailReg}", "password": f"{passwordReg}", "secretId": f"{secret}"}
+            newPass = {"username": f"{usernameReg}", "email": f"{emailReg}", "password": f"{passwordReg}",
+                       "secretId": f"{secret}"}
             pass_data.append(newPass)
             with open('pass.json', 'w') as wPass:
                 json.dump(pass_data, wPass, indent=4)
     success_message = "Register success"
     return render_template('index.html', successReg=success_message)
+
 
 @app.route('/changePass', methods=['GET', 'POST'])
 def changePass():
